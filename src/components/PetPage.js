1740e9211085c41a1f427f461e197798a99558d9
Ricardo Guerra <RikkuG491@gmail.com>
2024-02-26 11:26:26 -0500

    Added example code from the lecture

--- src/components/PetPage.js
+++ src/components/PetPage.js
@@ -1,5 +1,5 @@
 import PetList from "./PetList";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Search from './Search';
 import NewPetForm from "./NewPetForm";
 
@@ -7,6 +7,16 @@ function PetPage(){
 
     const [pets, setPets] = useState([])
     const [searchText, setSearchText] = useState("")
+
+    useEffect(() => {
+        fetch('http://localhost:4000/pets')
+        .then(response => response.json())
+        .then(petsData => setPets(petsData))
+
+        // console.log("Running the side effect code in useEffect()")
+    }, [])
+
+    // console.log("Rendering the component")
 
     const filteredPets = pets.filter(pet => {
         return pet.name.toUpperCase().includes(searchText.toUpperCase())
@@ -23,7 +33,18 @@ function PetPage(){
     }
 
     function addPet(newPet){
-        setPets([...pets, newPet])
+        // setPets([...pets, newPet])
+
+        fetch('http://localhost:4000/pets', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newPet)
+        })
+        .then(response => response.json())
+        .then(newPetData => setPets([...pets, newPetData]))
+
     }
 
     return (
