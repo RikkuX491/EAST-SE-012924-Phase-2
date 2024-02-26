20ab73db6e14c57e68d922f53ff739ba268b6e56
Ricardo Guerra <RikkuG491@gmail.com>
2024-02-26 13:54:15 -0500

    Created starter code and content for Lecture 6

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -8,11 +8,28 @@ function Pet({pet, deletePet}){
         setDisplayAnimalType(displayAnimalType => !displayAnimalType)
     }
 
+    function handleAdoptButtonClick(){
+        deletePet(pet.id)
+    }
+
     return (
         <li className="pet">
             <img src={pet.image} alt={pet.name}/>
             <h4 onClick={toggleDisplayAnimalType} className={displayAnimalType ? "display-animal-type" : ""}>{displayAnimalType ? pet.animal_type : pet.name}</h4>
-            <button onClick={() => deletePet(pet.id)} className="adopt-button">Adopt</button>
+            { true ?
+            <div className="button-div">
+                <button className="update-button">Update Pet</button>
+                <button className="like-button">0 Likes</button>
+                <br/>
+                <button onClick={handleAdoptButtonClick} className="adopt-button">Adopt</button>
+            </div> :
+            <form>
+                <input type="text" name="name" placeholder="Pet name" />
+                <input type="text" name="image" placeholder="Image URL" />
+                <input type="text" name="animal_type" placeholder="Animal type" />
+                <button className="save-changes-button" type="submit">Save Changes</button>
+            </form>
+            }
         </li>
     );
 }
