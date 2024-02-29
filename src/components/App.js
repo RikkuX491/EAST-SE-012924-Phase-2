953ee8b0a6e74872373d12228717e6c161a38a13
Ricardo Guerra <RikkuG491@gmail.com>
2024-02-29 01:25:11 -0500

    Created starter code and content for Lecture 7

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,93 @@
 import Header from "./Header";
-import PetPage from "./PetPage";
+import PetList from "./PetList";
+import { useState, useEffect } from "react";
+import NewPetForm from "./NewPetForm";
 
-function App() {
-  return (
-    <div className="app">
+function App(){
+
+    const [pets, setPets] = useState([])
+    // const [route, setRoute] = useState(window.location.pathname)
+
+    // let component = null
+    // if(route === "/"){
+    //   component = <PetList pets={pets} deletePet={deletePet} updatePet={updatePet}/>
+    // }
+    // else if(route === "/add_pet"){
+    //   component = <NewPetForm addPet={addPet}/>
+    // }
+
+    useEffect(() => {
+        fetch('http://localhost:4000/pets')
+        .then(response => response.json())
+        .then(petsData => setPets(petsData))
+    }, [])
+
+    function deletePet(id){
+        fetch(`http://localhost:4000/pets/${id}`, {
+            method: "DELETE"
+        })
+        .then(response => {
+            if(response.ok){
+                setPets((pets) => pets.filter(pet => {
+                    return pet.id !== id
+                }))
+            }
+            else{
+                alert("Error: Unable to delete pet!")
+            }
+        })
+    }
+
+    function addPet(newPet){
+        fetch('http://localhost:4000/pets', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({...newPet, likes: 0})
+        })
+        .then(response => response.json())
+        .then(newPetData => setPets([...pets, newPetData]))
+    }
+
+    function updatePet(id, petDataForUpdate){
+        fetch(`http://localhost:4000/pets/${id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(petDataForUpdate)
+        })
+        .then(response => response.json())
+        .then(updatedPet => setPets(pets => pets.map(pet => {
+            if(updatedPet.id === pet.id){
+                return updatedPet
+            }
+            else{
+                return pet
+            }
+        })))
+    }
+
+    // function navigate(event){
+    //   event.preventDefault()
+    //   window.history.pushState(null, "", event.target.href);
+    //   setRoute(window.location.pathname)
+    // }
+
+    return (
+      <div className="app">
+        {/* <nav>
+          <a onClick={(event) => navigate(event, "/")} href="/">Home</a>
+          <a onClick={(event) => navigate(event, "/add_pet")} href="/add_pet">Add Pet</a>
+        </nav> */}
         <Header/>
-        <PetPage/>
-    </div>
-  );
+        <NewPetForm addPet={addPet}/>
+        <PetList pets={pets} deletePet={deletePet} updatePet={updatePet}/>
+        {/* {component} */}
+      </div>
+    );
 }
 
 export default App;
--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,66 +1,17 @@
 import { useState } from "react";
 
-function Pet({pet, deletePet, updatePet}){
+function Pet({pet}){
 
     const [displayAnimalType, setDisplayAnimalType] = useState(false)
-    const [displayForm, setDisplayForm] = useState(false)
-    const [formData, setFormData] = useState({
-        name: "",
-        image: "",
-        animal_type: ""
-    })
 
     function toggleDisplayAnimalType(){
         setDisplayAnimalType(displayAnimalType => !displayAnimalType)
-    }
-
-    function handleAdoptButtonClick(){
-        deletePet(pet.id)
-    }
-
-    function toggleDisplayForm(){
-        setDisplayForm(displayForm => !displayForm)
-    }
-
-    function handleSubmit(event){
-        event.preventDefault()
-        
-        // console.log(formData)
-        // console.log(pet.id)
-
-        updatePet(pet.id, formData)
-
-        toggleDisplayForm()
-    }
-
-    function updateFormData(event){
-        setFormData({...formData, [event.target.name]: event.target.value})
-    }
-
-    function handleLikeButtonClick(){
-        // console.log({likes: pet.likes + 1})
-        const updatedLikesObject = {likes: pet.likes + 1}
-        updatePet(pet.id, updatedLikesObject)
     }
 
     return (
         <li className="pet">
             <img src={pet.image} alt={pet.name}/>
             <h4 onClick={toggleDisplayAnimalType} className={displayAnimalType ? "display-animal-type" : ""}>{displayAnimalType ? pet.animal_type : pet.name}</h4>
-            { !displayForm ?
-            <div className="button-div">
-                <button onClick={toggleDisplayForm} className="update-button">Update Pet</button>
-                <button onClick={handleLikeButtonClick} className="like-button">{pet.likes} Likes</button>
-                <br/>
-                <button onClick={handleAdoptButtonClick} className="adopt-button">Adopt</button>
-            </div> :
-            <form onSubmit={handleSubmit} className="edit-pet">
-                <input onChange={updateFormData} type="text" name="name" placeholder="Pet name" value={formData.name} />
-                <input onChange={updateFormData} type="text" name="image" placeholder="Image URL" value={formData.image} />
-                <input onChange={updateFormData} type="text" name="animal_type" placeholder="Animal type" value={formData.animal_type} />
-                <button type="submit">Save Changes</button>
-            </form>
-            }
         </li>
     );
 }
